ebeaa2a985ca1c8ece8b932fdc05005baeeeffd4
peizai85 <76763132@qq.com>
2024-08-26 21:59:42 +0800

    max

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 return [
-    'api_base_url' => 'http://192.168.17:8848/v1/devices', // 将此替换为实际的API接口地址
+    'api_base_url' => 'http://192.168.1.17:8848/v1/devices', // 将此替换为实际的API接口地址
     'api_key' => 'ivRvjl5achM6WUg05ioHyzSEntwrmz21EOVlheh1NczHvVkd',  // 如果需要API密钥
 ];
