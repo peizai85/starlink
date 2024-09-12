e7decb61d1db31053abc49c9cd91343bcf04ee03
peizai85 <76763132@qq.com>
2024-09-13 00:44:07 +0800

    max

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'api_base_url' => 'http://192.168.2.109:8848/v1/devices', // 将此替换为实际的API接口地址
+    'api_base_url' => 'https://starapi.hualucn.cn/v1/devices', // 将此替换为实际的API接口地址
     'api_key' => 'ivRvjl5achM6WUg05ioHyzSEntwrmz21EOVl1eh1NczHvVkd',  // 如果需要API密钥
     'devices_id' => 2,
 ];
