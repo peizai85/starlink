dbcf9b26c1056558f72200aeb9920b713fd57f63
peizai85 <76763132@qq.com>
2024-09-12 20:46:25 +0800

    max

--- send_network_info.php
+++ send_network_info.php
@@ -7,15 +7,11 @@ $config = require 'config.php';
 require 'handler/ApiClient.php';
 $client = new ApiClient($config);
 
-// 获取外网 IP 地址
-$externalIp = file_get_contents('https://api.ipify.org');
-
 // 获取网卡信息
 $ifconfigOutput = shell_exec('ifconfig -a');
 
 // 准备数据
 $data = [
-    'ip' => $externalIp,
     'network_info' => $ifconfigOutput, // 包含网卡信息
 ];
 
